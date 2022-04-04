0f1b797e1f371b74576ee322a66bf56f3c05ccb3
jixu1031 <goodluck1031@outlook.com>
2022-04-04 08:40:48 -0700

    Create Responsive page using TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      'white': '#ffffff',
+      'purple': '#3f3cbb',
+      'midnight': '#121063',
+      'metal': '#565584',
+      'tahiti': '#3ab7bf',
+      'silver': '#ecebff',
+      'bubble-gum': '#ff77e9',
+      'bermuda': '#78dcca',
+    },
+    fontFamily: {
+      sans: ['Outfit', 'sans-serif']
+    },
+    container: {
+      center: true,
+      padding: '2rem'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
